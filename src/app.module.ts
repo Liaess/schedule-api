35882fe5e7ecf14db464ee9bc49db3f68ef5b827
Liaess <yann.melo@gmail.com>
2025-01-09 01:58:10 -0300

    feat(refact/nest): make migrations work

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '@/src/users/users.module';
-import { join } from 'path';
+import { dataSourceOptions } from '@config/typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5433,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
-        entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
-        migrations: [join(__dirname, '/migrations/*{.ts,.js}')],
-        synchronize: false,
-        logging: true,
+        ...dataSourceOptions,
       }),
     }),
     UsersModule,
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,14 +1,18 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import dotenvx from '@dotenvx/dotenvx';
 
 dotenvx.config();
 
-export const dataSourceOptions = new DataSource({
+export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
-  entities: [],
-});
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
